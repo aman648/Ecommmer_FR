f811c2edf50306e65dc149e644da1108b9b78335
aman648 <amanveer648@gmail.com>
2026-02-07 01:27:41 +0800

    delvel

--- Frontend/ecommer_fr/src/Compontents/Welcome.js
+++ Frontend/ecommer_fr/src/Compontents/Welcome.js
@@ -1,8 +1,11 @@
 import React, {  useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function Welcome() {
     const navigator = useNavigate();
+    const [products, setProducts] = useState([]);
     const handlelogout = ()=>{
         if(localStorage.getItem("auth_token")){
             localStorage.removeItem("auth_token")
@@ -11,6 +14,20 @@ export default function Welcome() {
 
     }
     useEffect (()=>{
+        axios.get('http://127.0.0.1:5000/api/getproducts',{
+            headers:{
+                Authorization: `Bearer ${localStorage.getItem('auth_token')}`
+            }
+        }).then((res)=>{
+            console.log(res.data);
+            const data  = res.data;
+            
+          
+            setProducts(data['products']);
+        }).catch((err)=>{
+            console.log(err);
+        })
+      
 
 
     })
@@ -19,6 +36,20 @@ export default function Welcome() {
     <h1>Welcome</h1>
     <h3>Dasboard</h3>
     <button onClick={handlelogout}>Logout</button>
+    <div className='products'>
+        {products.map(
+            product => <div className='product' key={product.id}>
+                <h4>{product.name}</h4>
+                <p>{product.description}</p>
+                <p>{product.price}</p>
+            </div>
+        )}
+      
+                
+    </div>
+
+
+
     </>
   )
 }
