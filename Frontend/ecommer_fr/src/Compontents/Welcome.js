c752d8b2fb59ad29b51ad0ea57294dee2ed7618b
aman648 <amanveer648@gmail.com>
2026-02-19 02:16:11 +0530

    changes

--- Frontend/ecommer_fr/src/Compontents/Welcome.js
+++ Frontend/ecommer_fr/src/Compontents/Welcome.js
@@ -1,6 +1,6 @@
 import React, {  lazy, Suspense, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
-import axios, { Axios } from 'axios';
+import axios from 'axios';
 import { useState } from 'react';
 import Cartitems from './cartitems';
 
@@ -14,32 +14,70 @@ export default function Welcome() {
     const [cartItems, setCartItems] = useState([]);
     const cartitems = ()=>{
       //run code for adding it:
-        
-        
-        setCart(!cart);
-
-    }
-
-    useEffect (()=>{
-        Axios.get('http://127.0.0.1:5000/api/cartitems',{
+       const url = "http://127.0.0.1:5000/api/cartitems"
+       const user = {
+        user_id: localStorage.getItem("user_name")
+          
+       }
+        axios.post(url,user,{
             headers:{
-                Authorization: `Bearer ${localStorage.getItem('auth_token')}`
+                'Content-Type': 'application/json',
             }
-        }).then((res)=>{   // Fixed the syntax error in the then block
+        }).then((res)=>{
             console.log(res.data);
-            setCartItems(res.data);
+            const data  = res.data;
+            
+            setCartItems(data['cart_items']);
+            // setCartItems(res.data);
+
+            setCart(!cart);
         }).catch((err)=>{
             console.log(err);
         })
-    })
+
+    }
+
+    
     const removecartitems = (index)=>{
         const newcartitems = [...cartItems];
         newcartitems.splice(index,1);
-        setCartItems(newcartitems);
+        console.log("removing item at index", index);
+        console.log("new cart items", cartItems[index].product_id);
+        //make api call to remove the item from cart in backend
+        const user = {
+            user_id: localStorage.getItem("user_name"),
+            product_id: cartItems[index].product_id
+        }
+        const url = "http://127.0.0.1:5000/api/removecart"
+        axios.post(url,user,{
+            headers:{
+                'Content-Type': 'application/json',
+            }
+        }).then((res)=>{
+            setCartItems(newcartitems);
+        }).catch((err)=>{
+            console.log(err);
+        })
     }
     const addtocart = (product)=>{
-        setCartItems([...cartItems,product])
-
+        //api call to add the product to cart in backend
+        console.log("added to cart", product);
+        const user = {
+            user_id: localStorage.getItem("user_name"),
+            product_id: product.id
+        }
+        const url = "http://127.0.0.1:5000/api/addcart"
+        axios.post(url,user,{
+            headers:{
+                Authorization: `Bearer ${localStorage.getItem('auth_token')}`
+            }
+        }).then((res)=>{
+            console.log(res.data);
+            setCartItems([...cartItems,product])
+        }).catch((err)=>{
+            alert("Failed to add to cart");
+            console.log(err);
+        })
         
     }
 
@@ -88,7 +126,7 @@ export default function Welcome() {
       <Suspense fallback={<div>Loading...</div>}>
         {products.map(
             (product)=>(
-                <Products key={product.id} id={product.id} name={product.name} description={product.description} price={product.price} handlecart={addtocart} />
+                <Products key={product.id} id={product.product_id} name={product.name} description={product.description} price={product.price} handlecart={addtocart} />
             )
         )}
         </Suspense>          
